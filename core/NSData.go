193dfe4e9aaddf33dd47673f59dcbac3c7f03ac8
Jonas Faber <dev.jonas.faber@gmail.com>
2021-03-20 17:18:30 +0100

    Add means to retrieve NSData as []byte

--- core/NSData.go
+++ core/NSData.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"github.com/progrium/macdriver/objc"
+	"unsafe"
 )
 
 import "C"
@@ -16,4 +17,26 @@ type NSData struct {
 // https://developer.apple.com/documentation/foundation/nsdata/1547231-datawithbytes?language=objc
 func NSData_WithBytes(b []byte, length uint64) NSData {
 	return NSData{objc.Get("NSData").Send("dataWithBytes:length:", uintptr(C.CBytes(b)), length)}
+}
+
+// Length is the number of bytes contained by the data object.
+// https://developer.apple.com/documentation/foundation/nsdata/1416769-length?language=objc
+func (d NSData) Length() uint64 {
+	return d.Get("length").Uint()
+}
+
+// Bytes returns a pointer to the data object's contents.
+// https://developer.apple.com/documentation/foundation/nsdata/1410616-bytes?language=objc
+func (d NSData) Bytes() uintptr {
+	return d.Get("bytes").Pointer()
+}
+
+// GoBytes is a wrapper method to convert the data to go slice of bytes.
+func (d NSData) GoBytes() []byte  {
+	ptr := d.Bytes();
+	length := d.Length()
+	if length == 0 {
+		return nil
+	}
+	return C.GoBytes(unsafe.Pointer(ptr), C.int(length))
 }
