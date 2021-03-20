23be53b7e485526032dae17d1c24268c98ef6dea
Jonas Faber <dev.jonas.faber@gmail.com>
2021-03-20 17:20:00 +0100

    add DataForTypes example

--- cocoa/NSPasteboard_test.go
+++ cocoa/NSPasteboard_test.go
@@ -3,6 +3,8 @@ package cocoa_test
 import (
 	"fmt"
 	"github.com/progrium/macdriver/cocoa"
+	"io/ioutil"
+	"log"
 )
 
 func ExampleNSPasteboard(){
@@ -17,4 +19,31 @@ func ExampleNSPasteboard(){
 	// read data from pasteboard
 	fmt.Println(gp.StringForType(cocoa.NSPasteboardTypeString))
 	// Output: test-content
+}
+
+func ExampleNSPasteboard_DataForType() {
+	// get general pasteboard
+	gp := cocoa.NSPasteboard_GeneralPasteboard()
+	// check which is the best available type (list all the types you can to handle here)
+	pbType := gp.AvailableTypeFromArray([]cocoa.NSPasteboardType{cocoa.NSPasteboardTypePNG, cocoa.NSPasteboardTypeTIFF})
+	if pbType == "" {
+		// check which types are in the pasteboard right now
+		log.Printf("no matching type found. only found: %s \n", gp.Types())
+		return
+	}
+	// fetch data
+	data := gp.DataForType(pbType)
+	var bs []byte
+	// turn into go bytes
+	bs = data.GoBytes()
+
+	f, err := ioutil.TempFile("", "out-img")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_, err = f.Write(bs)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("png written to file: %s\n", f.Name())
 }
