549904576de9975f4c5f5b4505f9b303bbe5b2c9
Jonas Faber <dev.jonas.faber@gmail.com>
2021-03-20 17:18:01 +0100

    Add comments to NSData

--- core/NSData.go
+++ core/NSData.go
@@ -6,10 +6,14 @@ import (
 
 import "C"
 
+// NSData is a static byte buffer in memory.
+// https://developer.apple.com/documentation/foundation/nsdata?language=objc
 type NSData struct {
 	objc.Object
 }
 
+// NSData_WithBytes creates a data object containing a given number of bytes copied from a given buffer.
+// https://developer.apple.com/documentation/foundation/nsdata/1547231-datawithbytes?language=objc
 func NSData_WithBytes(b []byte, length uint64) NSData {
 	return NSData{objc.Get("NSData").Send("dataWithBytes:length:", uintptr(C.CBytes(b)), length)}
 }
