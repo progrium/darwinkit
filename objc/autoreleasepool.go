0a1f2e17bca5bf8c23443b6fe8bc91c55a8b0327
Matt Good <mgood@users.noreply.github.com>
2021-05-20 11:17:59 -0700

    use runtime.LockOSThread

--- objc/autoreleasepool.go
+++ objc/autoreleasepool.go
@@ -6,8 +6,13 @@ extern void objc_autoreleasePoolPop(void* pool);
 extern void* objc_autoreleasePoolPush(void);
 */
 import "C"
+import "runtime"
 
 func AutoreleasePool(body func()) {
+	// ObjC autoreleasepools are thread-local, so we need the body to be locked to
+	// the same OS thread.
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
 	pool := C.objc_autoreleasePoolPush()
 	defer C.objc_autoreleasePoolPop(pool)
 	body()
