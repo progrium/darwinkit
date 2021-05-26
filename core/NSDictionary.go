ff2aea315c754764838ebe6da59be651b9143c7e
Matt Good <mgood@users.noreply.github.com>
2021-05-26 15:16:17 -0700

    prototype for calling msgSend directly with CGo

--- core/NSDictionary.go
+++ core/NSDictionary.go
@@ -1,8 +1,23 @@
 package core
 
 import (
+	"unsafe"
+
 	"github.com/progrium/macdriver/objc"
 )
+
+/*
+#cgo LDFLAGS: -lobjc
+#define __OBJC2__ 1
+#include <objc/message.h>
+
+int (* send_NSDictionary_count)(id self, SEL _cmd) = (int(*)(id,SEL))objc_msgSend;
+
+int NSDictionary_count(void *id) {
+	return send_NSDictionary_count(id, (void *) sel_registerName("count"));
+}
+*/
+import "C"
 
 type NSDictionary struct {
 	objc.Object
@@ -20,4 +35,9 @@ func NSDictionary_Init(valueKeys ...interface{}) NSDictionary {
 
 func (d NSDictionary) ObjectForKey(key objc.Object) objc.Object {
 	return d.Send("objectForKey:", key)
+}
+
+func (d NSDictionary) Count() int {
+	x := C.NSDictionary_count(unsafe.Pointer(d.Pointer()))
+	return int(x)
 }
