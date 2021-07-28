ecb931a36a6b6e75379c86fa30fb8a1aefe99e71
Ethan Reesor <ethan.reesor@gmail.com>
2021-07-27 19:21:44 -0500

    Update NSNib interface

--- cocoa/NSNib.go
+++ cocoa/NSNib.go
@@ -9,11 +9,23 @@ type NSNib struct {
 	objc.Object
 }
 
-func NSNib_Init(name string, bundle NSBundle) NSNib {
+func NSNib_InitWithNibNamed_Bundle(name string, bundle NSBundle) NSNib {
 	return NSNib{objc.Get("NSNib").Alloc().Send("initWithNibNamed:bundle:",
 		core.String(name), bundle)}
 }
 
+func NSNib_InitWithNibData_Bundle(data core.NSData, bundle NSBundle) NSNib {
+	return NSNib{objc.Get("NSNib").Alloc().Send("initWithNibData:bundle:",
+		data, bundle)}
+}
+
+func (nib NSNib) InstantiateWithOwner_TopLevelObjects(owner objc.Object) (core.NSArray, bool) {
+	var ptr uintptr
+	ok := nib.Send("instantiateWithOwner:topLevelObjects:", owner, &ptr).Bool()
+	return core.NSArray{Object: objc.ObjectPtr(ptr)}, ok
+}
+
+// Deprecated: use InstantiateWithOwner_TopLevelObjects
 func (nib NSNib) InstantiateWithOwner(owner objc.Object) {
 	nib.Send("instantiateNibWithOwner:topLevelObjects:", owner, nil)
 }
