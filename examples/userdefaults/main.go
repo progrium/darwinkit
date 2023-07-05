32ce7c5d710d071ee4648a6a33f055df83b6435c
Travis Cline <travis.cline@gmail.com>
2023-07-05 11:38:09 -0700

    examples: Reflect naming change

--- examples/userdefaults/main.go
+++ examples/userdefaults/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	ud := core.NSUserDefaults_standardUserDefaults()
-	ud.SetURL_forKey_(core.URL("https://github.com/progrium/macdriver"), core.String("macdriver"))
-	u := ud.URLForKey_(core.String("macdriver"))
+	ud.SetURL_forKey(core.URL("https://github.com/progrium/macdriver"), core.String("macdriver"))
+	u := ud.URLForKey(core.String("macdriver"))
 	fmt.Println("looked up 'macdriver' key in NSUserDefaults and got:", u)
 }
