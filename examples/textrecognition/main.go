3037b7f18ac19a7c9e249b04027b7e4c740b0565
Travis Cline <travis.cline@gmail.com>
2023-07-09 19:45:07 -0700

    examples: drop textrecognitiion for now

--- examples/textrecognition/main.go
+++ examples/textrecognition/main.go
@@ -11,10 +11,10 @@ func main() {
 	u := core.NSURL_URLWithString(core.String("file:///Users/tmc/Desktop/screenshots/ss.png"))
 
 	opts := core.NSDictionary_New()
-	rh := vision.VNImageRequestHandler_Alloc().InitWithURLOptions_AsVNImageRequestHandler(u, opts)
+	rh := vision.VNImageRequestHandler_Alloc().InitWithURLOptions(u, opts)
 
 	req := vision.VNRecognizeTextRequest_Alloc().Init()
-	reqs := core.NSArray_WithObjects(req)
-	rh.PerformReq
-	fmt.Println(r)
+	reqs := core.NSArray_Alloc().Init()
+	fmt.Println(rh.PerformRequestsError(reqs, nsErr))
+	fmt.Println(nsErr)
 }
