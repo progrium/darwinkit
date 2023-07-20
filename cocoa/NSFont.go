9e9fecd54cb9309f73e05473058b964ea518502d
Travis Cline <travis.cline@gmail.com>
2023-07-20 13:24:16 -0700

    cocoa: Restore Font

--- cocoa/NSFont.go
+++ cocoa/NSFont.go
@@ -1,3 +1,14 @@
 package cocoa
 
+import (
+	"github.com/progrium/macdriver/core"
+)
+
 type NSFont struct{ gen_NSFont }
+
+func NSFont_Init(fontName string, size float64) NSFont {
+	return NSFont_FontWithNameSize(
+		fontName,
+		core.CGFloat(size),
+	)
+}
--- cocoa/init.go
+++ cocoa/init.go
@@ -8,3 +8,7 @@ import "C"
 func Color(r, g, b, a float64) NSColor {
 	return NSColor_Init(r, g, b, a)
 }
+
+func Font(fontName string, size float64) NSFont {
+	return NSFont_Init(fontName, size)
+}
