50477a9557ac8f7cfcac0406076977b66ec68de5
Jeff Lindsay <progrium@gmail.com>
2023-08-17 03:42:33 -0500

    dispatch: docs and api tweaks

--- dispatch/dispatch.go
+++ dispatch/dispatch.go
@@ -1,3 +1,8 @@
+// Execute code concurrently on multicore hardware by submitting work to dispatch queues managed by the system.
+//
+// [Apple Documentation]
+//
+// [AppleDocumentation]: https://developer.apple.com/documentation/dispatch?language=objc
 package dispatch
 
 // #cgo CFLAGS: -x objective-c
