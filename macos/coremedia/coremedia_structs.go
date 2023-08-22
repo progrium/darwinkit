e063b249205338d7768d223a1ef3d23366cd8c47
Jeff Lindsay <progrium@gmail.com>
2023-08-22 17:44:21 -0500

    macos: add missing types and placeholder structs to coremedia and corevideo

--- macos/coremedia/coremedia_structs.go
+++ macos/coremedia/coremedia_structs.go
@@ -6,8 +6,14 @@ import "unsafe"
 type TimeMapping struct{}
 type TimeRange struct{}
 type Time struct{}
+type VideoDimensions struct{}
 
 type SampleBufferRef unsafe.Pointer
 type BufferRef unsafe.Pointer
 type BufferQueueRef unsafe.Pointer
 type FormatDescriptionRef unsafe.Pointer
+type MetadataFormatDescriptionRef unsafe.Pointer
+type ClockRef unsafe.Pointer
+type TimebaseRef unsafe.Pointer
+type VideoFormatDescriptionRef unsafe.Pointer
+type AudioFormatDescriptionRef unsafe.Pointer
