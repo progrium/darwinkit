progrium/macdriver

generate: add partial struct support

generate/codegen/gen_struct.go
@@ -1,0 +1,19 @@
+package codegen
+
+import (
+	"github.com/progrium/macdriver/generate/typing"
+)
+
+// Struct is code generator for objective-c struct
+type Struct struct {
+	Type        *typing.StructType
+	Name        string // the first part of objc function name
+	GoName      string
+	Deprecated  bool // if has been deprecated
+	Suffix      bool // GoName conflicts so add suffix to this function
+	Description string
+	DocURL      string
+
+	goFuncName string
+	identifier string
+}

generate/struct.go
@@ -1,0 +1,36 @@
+package generate
+
+import (
+	"log"
+
+	"github.com/progrium/macdriver/generate/codegen"
+	"github.com/progrium/macdriver/generate/typing"
+)
+
+func (db *Generator) ToStruct(fw string, sym Symbol) *codegen.Struct {
+	// these structs have known declparse failures
+	knownIssues := map[string]bool{}
+	if knownIssues[sym.Name] {
+		_, err := sym.Parse()
+		log.Printf("skipping struct %s %s because of known issue: decl='%s' err='%v'\n", fw, sym.Name, sym.Declaration, err)
+		return nil
+	}
+	typ := db.TypeFromSymbol(sym)
+	styp, ok := typ.(*typing.StructType)
+	if !ok {
+		return nil
+	}
+	s := &codegen.Struct{
+		Name:        sym.Name,
+		GoName:      sym.Name,
+		Description: sym.Description,
+		DocURL:      sym.DocURL(),
+		Type:        styp,
+	}
+
+	return s
+
+}
+
+/*
+ */

progrium/darwinkit

generate/codegen/gen_struct.go
@@ -1,0 +1,19 @@
+package codegen
+
+import (
+	"github.com/progrium/darwinkit/generate/typing"
+)
+
+// Struct is code generator for objective-c struct
+type Struct struct {
+	Type        *typing.StructType
+	Name        string // the first part of objc function name
+	GoName      string
+	Deprecated  bool // if has been deprecated
+	Suffix      bool // GoName conflicts so add suffix to this function
+	Description string
+	DocURL      string
+
+	goFuncName string
+	identifier string
+}

generate/struct.go
@@ -1,0 +1,36 @@
+package generate
+
+import (
+	"log"
+
+	"github.com/progrium/darwinkit/generate/codegen"
+	"github.com/progrium/darwinkit/generate/typing"
+)
+
+func (db *Generator) ToStruct(fw string, sym Symbol) *codegen.Struct {
+	// these structs have known declparse failures
+	knownIssues := map[string]bool{}
+	if knownIssues[sym.Name] {
+		_, err := sym.Parse(db.Platform)
+		log.Printf("skipping struct %s %s because of known issue: decl='%s' err='%v'\n", fw, sym.Name, sym.Declaration, err)
+		return nil
+	}
+	typ := db.TypeFromSymbol(sym)
+	styp, ok := typ.(*typing.StructType)
+	if !ok {
+		return nil
+	}
+	s := &codegen.Struct{
+		Name:        sym.Name,
+		GoName:      sym.Name,
+		Description: sym.Description,
+		DocURL:      sym.DocURL(),
+		Type:        styp,
+	}
+
+	return s
+
+}
+
+/*
+ */